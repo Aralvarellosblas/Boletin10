da881f866a80c02664ea0585917e4ace43902a10
Aralvarellosblas <arturo.alvarellos@gmail.com>
2017-12-18 14:23:25 +0100

    Ejercicio Terminado

---
+++ Ejercicio10_1/src/ejercicio10_1/Ejercicio10_1.java
@@ -0,0 +1,12 @@
+package ejercicio10_1;
+/*
+ * @author Arturo
+ */
+public class Ejercicio10_1 {
+
+    public static void main(String[] args) {
+        Juego j1=new Juego();
+        j1.jugar();
+    }
+
+}
